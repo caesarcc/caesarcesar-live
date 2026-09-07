ec145884a925b266b858e41f0ad852b4239a404a
caesarcc <caesarcesar.rs@gmail.com>
2026-09-07 15:43:58 -0300

    feat: configure static export in Next.js and add GitHub Actions workflow for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true, // Obrigatório: GitHub Pages não possui servidor Node para otimizar imagens dinamicamente
+  },
+  trailingSlash: true, // Garante que as rotas gerem pastas com index.html, facilitando navegação direta
 };
 
 export default nextConfig;
